bd4694263ad471a46b9df81b900bbbfe98f707db
Carlo Frinolli <carlo.frinolli@nois3.it>
2016-08-22 15:04:05 +0200

    live dangerously

--- app.js
+++ app.js
@@ -16,3 +16,31 @@ app.get('/', function(req, res) {
 });
 
 module.exports = app;
+express.Router().post('/smooch/webhooks', (req, res) => {
+    const smoochPayload = req.body.postbacks[0].action.payload;
+    const userId = req.body.appUser._id;
+
+    if (smoochPayload === 'here_for_the_bot') {
+        smoochApi.conversations.sendMessage(userId, {
+            text: 'Learn more on our implementation and why, here: http://nois3.it/blog',
+            role: 'appMaker'
+        });
+    }
+
+    if (smoochPayload === 'learn_more_about_nois3') {
+        smoochApi.conversations.sendMessage(userId, {
+            text: 'Learn more about nois3 here: http://nois3.it/about',
+            role: 'appMaker'
+        });
+    }
+
+
+    if (smoochPayload === 'wud') {
+        smoochApi.conversations.sendMessage(userId, {
+            text: 'Learn more about World Usability Day Rome 2016: http://wudrome.it',
+            role: 'appMaker'
+        });
+    }
+
+    res.end();
+});
